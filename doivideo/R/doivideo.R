d0f4d173d49c36a21f9c8addae33c858766c0c79
Tom Morrell <tmorrell@caltech.edu>
2020-02-18 13:29:49 -0800

    Add deps

---
+++ doivideo/R/doivideo.R
@@ -0,0 +1,53 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+doivideo <- function(doi,index, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    doi = doi,
+    index = index
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'doivideo',
+    x,
+    width = width,
+    height = height,
+    package = 'doivideo',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for doivideo
+#'
+#' Output and render functions for using doivideo within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a doivideo
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name doivideo-shiny
+#'
+#' @export
+doivideoOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'doivideo', width, height, package = 'doivideo')
+}
+
+#' @rdname doivideo-shiny
+#' @export
+renderDoivideo <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, doivideoOutput, env, quoted = TRUE)
+}
