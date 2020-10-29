c3871be18a9e22d57b807cccb2507d65f98e60b1
KianBadie <kianbadie@gmail.com>
2020-10-29 12:42:19 -0700

    Update doivideo to use manual videojs setup.

--- doivideo/inst/htmlwidgets/doivideo.js
+++ doivideo/inst/htmlwidgets/doivideo.js
@@ -29,14 +29,21 @@ HTMLWidgets.widget({
         }
         </style>
         <div class="video">
-        <video class="video-js vjs-default-skin vjs-big-play-centered" width="${width}" height="${height}" poster="${x.screenshot}" controls preload="auto" responsive=true id="my-video" data-setup='{"fluid": true}'>
-        <source src="${obj.media_url}" type='${obj.media_type}'>
+        <video class="video-js vjs-default-skin vjs-big-play-centered" width="${width}" height="${height}" id="my-video">
+        <source src="${obj.media_url}" type="${obj.media_type}">
         <p class="vjs-no-js">
           To view this video please enable JavaScript, and consider upgrading to a web browser that
           <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
         </p>
       </video>
       </div>`;
+      videojs(document.querySelector(`#${el.id} video`), {
+        fluid: true,
+        controls: true,
+        preload: "auto",
+        responsive: true,
+        poster: `${x.screenshot}`
+      });
         });
       },
 
