95e01d4173777f58b697b13c82c9c1e2663f6f5e
KianBadie <kianbadie@gmail.com>
2021-01-20 13:55:32 -0800

    Add styles/javascript to get rid of scrolling bugs for mobile.

--- js/mobile-controls.js
+++ js/mobile-controls.js
@@ -1,3 +1,17 @@
+function addPageContentPadding() {
+    let topPadding = document.querySelector("header").offsetHeight;
+    let bottomPadding = document.querySelector(".nav-controls-mobile").offsetHeight;
+    let contentContainer = document.querySelector(".book-page-content");
+    contentContainer.style["padding-top"] = `${topPadding}px`;
+    contentContainer.style["padding-bottom"] = `${bottomPadding}px`;
+}
+
+function removePageContentPadding() {
+    let contentContainer = document.querySelector(".book-page-content");
+    contentContainer.style["padding-top"] = "initial";
+    contentContainer.style["padding-bottom"] = "initial";
+}
+
 function toggleView(el) {
     // Check current state of screen
     let mobileView = window.sessionStorage.getItem("mobileView");
@@ -8,9 +22,11 @@ function toggleView(el) {
         if(el.value == "text") {
             videoContainer.style.display = "none";
             textContainer.style.display = "flex";
+            addPageContentPadding();
         } else if(el.value == "video") {
             videoContainer.style.display = "flex";
             textContainer.style.display = "none";
+            removePageContentPadding();
         }
     }
 }
