8b215719556fb60c7f4ce3bd9871039097b76616
KianBadie <kianbadie@gmail.com>
2021-01-21 13:14:40 -0800

    Add styles/script to render section pages on mobile landscape screens.

--- js/mobile-controls.js
+++ js/mobile-controls.js
@@ -1,9 +1,16 @@
+window.addEventListener("orientationchange", fixPadding);
+
 function addPageContentPadding() {
     let topPadding = document.querySelector("header").offsetHeight;
     let bottomPadding = document.querySelector(".page-controls-mobile").offsetHeight;
     let contentContainer = document.querySelector(".book-page-content");
     contentContainer.style["padding-top"] = `${topPadding}px`;
-    contentContainer.style["padding-bottom"] = `${bottomPadding}px`;
+    // Add bottom padding only on portrait
+    if(window.innerHeight > window.innerWidth) {
+        contentContainer.style["padding-bottom"] = `${bottomPadding}px`;
+    } else {
+        contentContainer.style["padding-bottom"] = "0";
+    }
 }
 
 function removePageContentPadding() {
@@ -24,12 +31,46 @@ function toggleView(el) {
             videoContainer.style.display = "none";
             textContainer.style.display = "flex";
             bookPageContainer.style.height = "initial";
+            if(window.innerHeight < window.innerWidth) {
+                let pageControls = document.querySelector(".page-controls-mobile");
+                let contentContainer = document.querySelector(".book-page-content");
+                pageControls.style.position = "relative";
+                contentContainer.style["padding-bottom"] = "0";
+            }
             addPageContentPadding();
         } else if(el.value == "video") {
             videoContainer.style.display = "flex";
             textContainer.style.display = "none";
             bookPageContainer.style.height = "100%";
+            if(window.innerHeight < window.innerWidth) {
+                let pageControls = document.querySelector(".page-controls-mobile");
+                pageControls.style.position = "fixed";
+            }
             removePageContentPadding();
+        }
+    }
+}
+
+// Recalculate text padding/components on orientation changes
+function fixPadding(event) {
+    let header = document.querySelector("header");
+    let contentContainer = document.querySelector(".book-page-content");
+    let textContainer = document.getElementById("textContent");
+    let bottomPadding = document.querySelector(".page-controls-mobile").offsetHeight;
+    let pageControls = document.querySelector(".page-controls-mobile");
+
+    // Reduce header size and remove bottom padding for landscape
+    if(window.innerHeight > window.innerWidth) {
+        if(textContainer.style.display == "flex") {
+            header.style.height = "56px";
+            pageControls.style.position = "relative";
+            contentContainer.style["padding-top"] = "56px";
+            contentContainer.style["padding-bottom"] = "0";
+        }
+    } else {
+        if(textContainer.style.display == "flex") {
+            pageControls.style.position = "fixed";
+            contentContainer.style["padding-bottom"] = `${bottomPadding}px`;
         }
     }
 }
