716817a6db5931952095166d0274c85da24bac5a
KianBadie <kianbadie@gmail.com>
2021-01-25 15:54:46 -0800

    Begin refactor for current mobile code. Breaks other pages for mobile, but will fix in following commits: 1. Remove fixPadding event listener and temporarily remove contents of renderVideo and renderText in mobile-controls.js. 2. Make text the defaul display on section pages. Move away from css rules that declare height and implement block displays. Make modals fullscreen on portrait. 3. Change nav icons on page controls and switch order of text/video buttons.

--- js/mobile-controls.js
+++ js/mobile-controls.js
@@ -1,4 +1,3 @@
-window.addEventListener("orientationchange", fixPadding);
 window.addEventListener("orientationchange", fixDropdownHeight);
 if(video) video.removeEventListener("play", shelfOnFirstPlay);
 
@@ -17,60 +16,11 @@ function toggleView(el) {
 }
 
 function renderText() {
-    let videoContainer = document.getElementById("nonTextContent");
-    let textContainer = document.getElementById("textContent");
-    let bookPageContainer = document.querySelector(".book-page-container");
-    let contentContainer = document.querySelector(".book-page-content");
-    let topPadding = document.querySelector("header").offsetHeight;
-
-    videoContainer.style.display = "none";
-    textContainer.style.display = "flex";
-    bookPageContainer.style.height = "initial";
-    contentContainer.style["padding-top"] = `${topPadding}px`;
-    if(window.innerHeight < window.innerWidth) {
-        let pageControls = document.querySelector(".page-controls-mobile");
-        let contentContainer = document.querySelector(".book-page-content");
-        pageControls.style.position = "relative";
-        contentContainer.style["padding-bottom"] = "0";
-    } else {
-        let bottomPadding = document.querySelector(".page-controls-mobile").offsetHeight;
-        contentContainer.style["padding-bottom"] = `${bottomPadding}px`;
-    }
+    console.log("Rendering text");
 }
 
 function renderVideo() {
-    let videoContainer = document.getElementById("nonTextContent");
-    let textContainer = document.getElementById("textContent");
-    let bookPageContainer = document.querySelector(".book-page-container");
-    let contentContainer = document.querySelector(".book-page-content");
-
-    videoContainer.style.display = "flex";
-    textContainer.style.display = "none";
-    bookPageContainer.style.height = "100%";
-    contentContainer.style["padding-top"] = "initial";
-    contentContainer.style["padding-bottom"] = "initial";
-    if(window.innerHeight < window.innerWidth) {
-        let pageControls = document.querySelector(".page-controls-mobile");
-        pageControls.style.position = "fixed";
-    }
-}
-
-// Recalculate text padding/components on orientation changes
-function fixPadding(event) {
-    let textContainer = document.getElementById("textContent");
-    if(!textContainer) return;
-    if(textContainer.style.display == "flex") {
-        let pageControls = document.querySelector(".page-controls-mobile");
-        let contentContainer = document.querySelector(".book-page-content");
-        if(window.innerHeight > window.innerWidth) {
-            pageControls.style.position = "relative";
-            contentContainer.style["padding-bottom"] = "0";
-        } else {
-            let bottomPadding = document.querySelector(".page-controls-mobile").offsetHeight;
-            pageControls.style.position = "fixed";
-            contentContainer.style["padding-bottom"] = `${bottomPadding}px`;
-        }
-    }
+    console.log("Rendering video");
 }
 
 function closeModalMobile(el) {
