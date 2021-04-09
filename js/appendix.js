ec5e62925007fa52bf5579260aea0afcd4d40f3b
KianBadie <KianBadie@users.noreply.github.com>
2021-04-09 22:30:53 +0000

    deploy: a396df6df12c811a9281f7873c8729a213d55b99

--- js/appendix.js
+++ js/appendix.js
@@ -65,3 +65,148 @@ function toggleListDropdown(el) {
         list.removeAttribute("showing");
     }
 }
+
+let treeViewer = document.querySelector(".book-appendix-tree-viewer");
+let treeGraphic;
+let zoomInButton;
+let currScale;
+let zoomFactor;
+let tx;
+let ty;
+let hidePopUpCalls;
+let currSpeciesEntry;
+if(treeViewer) {
+    treeGraphic = treeViewer.querySelector("svg[data-id='treeSvg']");
+    zoomInButton = treeViewer.querySelector("#treeZoomIn");
+    zoomOutButton = treeViewer.querySelector("#treeZoomOut");
+    currScale = 1;
+    zoomFactor = 1.05;
+    tx = 0;
+    ty = 0;
+
+    treeViewer.addEventListener("wheel", function(event) {
+        event.preventDefault();
+
+        let currZoomFactor = zoomFactor;
+        if(event.deltaY >= 0) currZoomFactor = 1/zoomFactor;  
+        currScale *= currZoomFactor;
+
+        // Calculate displacement of zooming position
+        let centerX = (treeViewer.getBoundingClientRect().right - treeViewer.getBoundingClientRect().x) / 2;
+        let centerY = (treeViewer.getBoundingClientRect().bottom - treeViewer.getBoundingClientRect().y) / 2;
+        let posX = (event.pageX - treeViewer.getBoundingClientRect().x) - centerX;
+        let posY = (event.pageY - treeViewer.getBoundingClientRect().y) - centerY;
+        calcTransform(posX, posY, currZoomFactor);
+    });
+
+    treeViewer.addEventListener("mousedown", function(event) {
+        let centerX = (treeViewer.getBoundingClientRect().right - treeViewer.getBoundingClientRect().x) / 2;
+        let centerY = (treeViewer.getBoundingClientRect().bottom - treeViewer.getBoundingClientRect().y) / 2;
+        let posX = (event.pageX - treeViewer.getBoundingClientRect().x) - centerX;
+        let posY = (event.pageY - treeViewer.getBoundingClientRect().y) - centerY;
+
+        treeViewer.addEventListener("mousemove", trackMouse);
+
+        treeViewer.addEventListener("mouseup", removeTracking, { once: true });
+
+        treeViewer.addEventListener("mouseleave", removeTracking, { once: true });
+
+        function trackMouse(event) {
+            event.preventDefault();
+            let currX = (event.pageX - treeViewer.getBoundingClientRect().x) - centerX;
+            let currY = (event.pageY - treeViewer.getBoundingClientRect().y) - centerY;
+            let dx = posX - currX;
+            let dy = posY - currY;
+            let newTx = tx - dx;
+            let newTy = ty - dy;
+
+            treeGraphic.style.transform = `matrix(${currScale}, 0, 0, ${currScale}, ${newTx}, ${newTy})`;
+            tx = newTx;
+            ty = newTy; 
+            posX = currX;
+            posY = currY;
+        }
+
+        function removeTracking() {
+            treeViewer.removeEventListener("mousemove", trackMouse);
+        }
+    });
+
+    zoomInButton.addEventListener("click", function(event) {
+        currScale *= zoomFactor;
+        calcTransform(0, 0, zoomFactor);
+    });
+
+    zoomOutButton.addEventListener("click", function(event) {
+        currScale *= 1/zoomFactor;
+        calcTransform(0, 0, 1/zoomFactor);
+    });
+
+    function calcTransform(posX, posY, zoomF) {
+        let dx = (posX - tx) * (zoomF - 1);
+        let dy = (posY - ty) * (zoomF - 1);
+        let newTx = tx - dx;
+        let newTy = ty - dy;
+
+        treeGraphic.style.transform = `matrix(${currScale}, 0, 0, ${currScale}, ${newTx}, ${newTy})`;
+        tx = newTx;
+        ty = newTy;
+    }
+
+    let speciesEntries = document.querySelectorAll(".book-appendix-tree-species-entry");
+    let currPopUp = undefined;
+    for(let speciesEntry of speciesEntries) {
+        let speciesId = speciesEntry.getAttribute("id");
+        let popUp = document.querySelector(`.book-appendix-tree-section-list[data-species='${speciesId}']`);
+        if(!popUp) continue;
+        hidePopUpCalls = [];
+        speciesEntry.addEventListener("mouseenter", function(event) {
+            if(currPopUp) currPopUp.style.display = "none";
+            if(currSpeciesEntry) currSpeciesEntry.style["text-decoration"] = "none";
+            currPopUp = popUp;
+            currSpeciesEntry = speciesEntry;
+            speciesEntry.style["text-decoration"] = "underline";
+            clearHideCalls();
+            popUp.removeEventListener("mouseenter", popUpHandleHover);
+            popUp.removeEventListener("mouseleave", popUpHandleLeave);
+            popUp.style.display = "block";
+            popUp.style.left = `${event.pageX}px`;
+            popUp.style.top = `${event.pageY}px`;
+
+            speciesEntry.addEventListener("mouseleave", function(event) {
+                hidePopUpCalls.push(hidePopUp(popUp));
+            }, {once: true});
+
+            popUp.addEventListener("mouseenter", popUpHandleHover);
+            popUp.addEventListener("mouseleave", popUpHandleLeave);
+
+        });
+
+        function clearHideCalls() {
+            for(let hidePopUpCall of hidePopUpCalls) {
+                window.clearTimeout(hidePopUpCall);
+            }
+        }
+
+        let popUpHandleHover = function() {
+            clearHideCalls();
+            popUp.setAttribute("data-hover", "true");
+        }
+
+        let popUpHandleLeave = function() {
+            popUp.setAttribute("data-hover", "false");
+            hidePopUpCalls.push(hidePopUp(popUp));
+        }
+
+        function hidePopUp(popUp) {
+            let timeoutNum = setTimeout(function() {
+                if(popUp.getAttribute("data-hover") != "true") {
+                    popUp.style.display = "none";
+                    currSpeciesEntry.style["text-decoration"] = "none";
+                }
+            }, 1000);
+            return timeoutNum;
+        }
+    }
+            
+}
