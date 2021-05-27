b7a06bb3ba66da599acd3a08a67e9350a38df8d1
KianBadie <kianbadie@gmail.com>
2021-05-27 11:31:23 -0700

    Remove molstar viewer from image tab and instead show fixed viewer on screen when structure text is clicked.

--- js/viewers.js
+++ js/viewers.js
@@ -6,8 +6,8 @@ for(let molstarApp of molstarApps) {
 
 function initializeMolstarApp(viewerEl) {
     let id = viewerEl.getAttribute("id");
-    let playerId = viewerEl.getAttribute("data-player");
-    let imageBtn = document.querySelector(`button[data-player='${playerId}'][value='image']`);
+    let pdb = viewerEl.getAttribute("data-pdb");
+    let link = document.querySelector(`.video-citation-value[data-pdb='${pdb}']`);
     let viewer = new molstar.Viewer(id, {
         layoutIsExpanded: false,
         layoutShowControls: false,
@@ -23,13 +23,32 @@ function initializeMolstarApp(viewerEl) {
         pdbProvider: 'rcsb',
         emdbProvider: 'rcsb',
     });
-    viewer.loadPdb("6UEW");
+    viewer.loadPdb(pdb);
 
-    imageBtn.addEventListener("click", () => resizeContainer());
-    window.addEventListener("resize", () => resizeContainer());
+    window.addEventListener("resize", positionViewer);
+    link.addEventListener("click", showViewer);
 
-    function resizeContainer() {
-        viewerEl.style.height = `${viewerEl.offsetWidth * 0.5625}px`;
+    function showViewer() {
+        viewerEl.style.display = "block";
+        positionViewer();
+    }
+
+    function positionViewer() {
+        let header = document.querySelector("header");
+        let footer = document.querySelector("footer");
+        let posTop = header.offsetHeight + ((footer.getBoundingClientRect().top - header.getBoundingClientRect().bottom) / 2);
+        let aspectRatio = 0.5625;
+        let availHeight = (footer.getBoundingClientRect().top - header.getBoundingClientRect().bottom) - 50;
+        let availWidth = window.innerWidth - 100;
+        let desiredWidth = availHeight / aspectRatio;
+        if(desiredWidth <= availWidth) {
+            viewerEl.style.width = `${desiredWidth}px`;
+            viewerEl.style.height = `${desiredWidth * aspectRatio}px`;
+        } else {
+            viewerEl.style.width = `${availWidth}px`;
+            viewerEl.style.height = `${availWidth * aspectRatio}px`;
+        }
+        viewerEl.style.top = `${posTop}px`;
         viewer.handleResize();
     }
 }
