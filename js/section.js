572c51845755c97699822ad381596bc11b1ffcd0
KianBadie <kianbadie@gmail.com>
2021-07-05 10:49:39 -0700

    Replace ch2 summary menu svgs to have labeled numbers and formatted text. Make translate distance for summary menu dynamic.

--- js/section.js
+++ js/section.js
@@ -1086,13 +1086,14 @@ if(document.querySelector(".summary-menu")) {
         let menuCenterY = summaryMenu.getBoundingClientRect().bottom - ((summaryMenu.getBoundingClientRect().bottom - summaryMenu.getBoundingClientRect().top) / 2);
         let itemCordX = menuItem.getBoundingClientRect().right - ((menuItem.getBoundingClientRect().right - menuItem.getBoundingClientRect().left) / 2);
         let itemCordY = menuItem.getBoundingClientRect().bottom - ((menuItem.getBoundingClientRect().bottom - menuItem.getBoundingClientRect().top) / 2);
-        let tx = (itemCordX > menuCenterX) ? 15 : -15;
-        let ty = (itemCordY > menuCenterY) ? 15 : -15;
+        let translateDist = focusTranslateRatio * menuContainer.clientWidth;
+        let tx = (itemCordX > menuCenterX) ? translateDist : -translateDist;
+        let ty = (itemCordY > menuCenterY) ? translateDist : -translateDist;
         if(currentOpened) deactivateMenuPart({ target: currentOpened });
         partGraphic.style.transform = `scale(1.125) translate(${tx}px, ${ty}px)`;
         partText.style.left = `${menuCenterX}px`;
         partText.style.top = `${menuCenterY}px`;
-        partText.style.width = `${summaryMenu.offsetWidth * 0.5}px`;
+        partText.style.width = `${menuContainer.offsetWidth * 0.7}px`;
         partText.classList.remove("summary-menu__li-text--hidden");
         menuItem.classList.add("summary-menu__li--active");
     };
@@ -1121,6 +1122,7 @@ if(document.querySelector(".summary-menu")) {
     let summaryMenu = document.querySelector(".summary-menu");
     let menuContainer = summaryMenu.querySelector(".summary-menu__container");
     let menuItems = summaryMenu.querySelectorAll(".summary-menu__li");
+    let focusTranslateRatio = 0.0215;
     resizeMenu();
     window.addEventListener("resize", resizeMenu);
 
