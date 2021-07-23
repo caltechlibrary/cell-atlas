182e7f31f32495aabb67aed59c87351737231fae
KianBadie <kianbadie@gmail.com>
2021-07-23 12:38:43 -0700

    Add generated sitemap and fix location urls.

--- _createSitemap.py
+++ _createSitemap.py
@@ -9,7 +9,7 @@ lines.append('<?xml version="1.0" encoding="UTF-8"?>\n')
 lines.append('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
 for file in files:
     lines.append('\t<url>\n')
-    lines.append('\t\t<loc>{}</loc>\n'.format(file))
+    lines.append('\t\t<loc>{}/{}</loc>\n'.format(host, file))
     lines.append('\t</url>\n')
 lines.append('</urlset>')
 
