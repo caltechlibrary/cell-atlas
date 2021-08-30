0a605b43a2fb2a177b9e3e9b5ee0475a4b348493
KianBadie <kianbadie@gmail.com>
2021-08-30 15:50:30 -0700

    Add classes to add hidden visibility to main seciton-text container and unshelve button.

--- js/SectionText.js
+++ js/SectionText.js
@@ -2,9 +2,9 @@ let SectionText = {
 
     settings: {
         root: document.querySelector(".section-text"),
-        mainContainer: document.querySelector(".section-text__main-container"),
-        shelveBtn: document.querySelector(".section-text__shelve-btn"),
-        unshelveBtn: document.querySelector(".section-text__unshelve-btn")
+        mainContainer: document.querySelector(".section-text .section-text__main-container"),
+        shelveBtn: document.querySelector(".section-text .section-text__shelve-btn"),
+        unshelveBtn: document.querySelector(".section-text .section-text__unshelve-btn")
     },
 
     init: function() {
@@ -20,9 +20,13 @@ let SectionText = {
         // Check if transitions are enabled
         if(document.querySelector("body").classList.contains("preload")) {
             SectionText.settings.mainContainer.classList.add("section-text__main-container--shelved");
+            SectionText.settings.mainContainer.classList.add("section-text__main-container--hidden");
+            SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--hidden");
             SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--shelved");
         } else {
             SectionText.settings.mainContainer.addEventListener("transitionend", function() {
+                SectionText.settings.mainContainer.classList.add("section-text__main-container--hidden");
+                SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--hidden");
                 SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--shelved");
             }, { once: true });
             SectionText.settings.mainContainer.classList.add("section-text__main-container--shelved");
@@ -33,9 +37,13 @@ let SectionText = {
         // Check if transitions are enabled
         if(document.querySelector("body").classList.contains("preload")) {
             SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--shelved");
+            SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--hidden");
+            SectionText.settings.mainContainer.classList.remove("section-text__main-container--hidden");
             SectionText.settings.mainContainer.classList.remove("section-text__main-container--shelved");
         } else {
             SectionText.settings.unshelveBtn.addEventListener("transitionend", function() {
+                SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--hidden");
+                SectionText.settings.mainContainer.classList.remove("section-text__main-container--hidden");
                 SectionText.settings.mainContainer.classList.remove("section-text__main-container--shelved");
             }, { once: true });
             SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--shelved");
