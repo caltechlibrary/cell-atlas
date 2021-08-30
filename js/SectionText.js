642095821d66e44b41e787b02432f9ea435db36d
KianBadie <kianbadie@gmail.com>
2021-08-30 15:33:20 -0700

    Move section-text component to its own template and place main section-text elements in seperate sub container.

--- js/SectionText.js
+++ js/SectionText.js
@@ -2,6 +2,7 @@ let SectionText = {
 
     settings: {
         root: document.querySelector(".section-text"),
+        mainContainer: document.querySelector(".section-text__main-container"),
         shelveBtn: document.querySelector(".section-text__shelve-btn"),
         unshelveBtn: document.querySelector(".section-text__unshelve-btn")
     },
@@ -18,26 +19,26 @@ let SectionText = {
     shelveText: function() {
         // Check if transitions are enabled
         if(document.querySelector("body").classList.contains("preload")) {
-            SectionText.settings.root.classList.add("section-text--shelved");
-            SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--on-screen");
+            SectionText.settings.mainContainer.classList.add("section-text__main-container--shelved");
+            SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--shelved");
         } else {
-            SectionText.settings.root.addEventListener("transitionend", function() {
-                SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--on-screen");
+            SectionText.settings.mainContainer.addEventListener("transitionend", function() {
+                SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--shelved");
             }, { once: true });
-            SectionText.settings.root.classList.add("section-text--shelved");
+            SectionText.settings.mainContainer.classList.add("section-text__main-container--shelved");
         }
     },
 
     unshelveText: function() {
         // Check if transitions are enabled
         if(document.querySelector("body").classList.contains("preload")) {
-            SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--on-screen");
-            SectionText.settings.root.classList.remove("section-text--shelved");
+            SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--shelved");
+            SectionText.settings.mainContainer.classList.remove("section-text__main-container--shelved");
         } else {
             SectionText.settings.unshelveBtn.addEventListener("transitionend", function() {
-                SectionText.settings.root.classList.remove("section-text--shelved");
+                SectionText.settings.mainContainer.classList.remove("section-text__main-container--shelved");
             }, { once: true });
-            SectionText.settings.unshelveBtn.classList.remove("section-text__unshelve-btn--on-screen");
+            SectionText.settings.unshelveBtn.classList.add("section-text__unshelve-btn--shelved");
         }
     }
 
