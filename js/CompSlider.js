4f2ca3f2dc9fec976e42a5a308b8abadb958c477
KianBadie <kianbadie@gmail.com>
2021-09-24 14:44:11 -0700

    Add image sliding functionality to comparison slider.

--- js/CompSlider.js
+++ js/CompSlider.js
@@ -4,6 +4,8 @@ let CompSlider = function(root) {
     let beforeSrc = beforeImg.getAttribute("data-src");
     let afterImg = root.querySelector(".comp-slider__after-img");
     let afterSrc = afterImg.getAttribute("data-src");
+    let slider = root.querySelector(".comp-slider__slider");
+    let sliderInput = root.querySelector(".comp-slider__input");
     let offline = root.getAttribute("data-offline");
 
     let init = function() {
@@ -28,9 +30,56 @@ let CompSlider = function(root) {
         let totalBorderHeight = parseFloat(window.getComputedStyle(parentMediaContainer)["borderTopWidth"]) + parseFloat(window.getComputedStyle(parentMediaContainer)["borderBottomWidth"]);
         let maxHeight = (sectionContainer.offsetHeight * mediaViewerMaxHeightPercent) - (mediaViewerTabContainer.offsetHeight + totalBorderHeight);
         beforeImg.style["max-height"] = `${maxHeight}px`;
-    }
+    };
+
+    let initImgSliding = function(event) {
+        event.preventDefault();
+        window.addEventListener("mousemove", onSlide);
+        window.addEventListener("touchmove", onSlide);
+        window.addEventListener("mouseup", endImgSliding);
+        window.addEventListener("touchend", endImgSliding);
+    };
+
+    let onSliderManualInput = function(event) {
+        setCompSliderPercentage(sliderInput.value);
+    };
+
+    let onSlide = function(event) {
+        let posPercentX = getPosX(event);
+        setCompSliderPercentage(posPercentX);
+    };
+
+    let getPosX = function(event) {
+        let boundingRect = root.getBoundingClientRect();
+        let pageX = ("pageX" in event) ? event.pageX : event.changedTouches[0].pageX;
+        let compSliderX;
+        if(pageX < boundingRect.left) {
+            compSliderX = 0;
+        } else if(pageX > boundingRect.right) {
+            compSliderX = boundingRect.right - boundingRect.left;
+        } else {
+            compSliderX = pageX - boundingRect.left;
+        }
+        return (compSliderX / root.offsetWidth) * 100;
+    };
+
+    let setCompSliderPercentage = function(percentage) {
+        slider.style.left = `${percentage}%`;
+        afterImg.style.width = `${percentage}%`;
+        sliderInput.value = percentage;
+    };
+
+    let endImgSliding = function(event) {
+        window.removeEventListener("mousemove", onSlide);
+        window.removeEventListener("touchmove", onSlide);
+        window.removeEventListener("mouseup", endImgSliding);
+        window.removeEventListener("touchend", endImgSliding);
+    };
 
     init();
     if(root.classList.contains("comp-slider--main-section")) window.addEventListener("resize", updateBeforeImgMaxHeight);
+    slider.addEventListener("mousedown", initImgSliding);
+    slider.addEventListener("touchstart", initImgSliding);
+    sliderInput.addEventListener("input", onSliderManualInput);
 
 }
