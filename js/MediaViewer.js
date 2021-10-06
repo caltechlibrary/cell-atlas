9efb7aa09b40f8be4daa892db35c42c3c8aa7c25
KianBadie <kianbadie@gmail.com>
2021-10-06 11:29:03 -0700

    Add non-functional fullscreen button to media viewer that only chages visual state.

--- js/MediaViewer.js
+++ js/MediaViewer.js
@@ -3,6 +3,7 @@ let MediaViewer = function(root) {
     let tabContainer = root.querySelector(".media-viewer__tab-container");
     let videoPlayer = root.querySelector(".book-section-video-player");
     let compSlider = root.querySelector(".comp-slider");
+    let fullscreenBtn = root.querySelector(".media-viewer__fullscreen-btn");
 
     let handleTabContainerClick = function(event) {
         let tabBtn = event.target.closest(".media-viewer__tab-btn");
@@ -19,16 +20,38 @@ let MediaViewer = function(root) {
         if(mediaType == "vid") {
             compSlider.classList.add("comp-slider--hidden");
             videoPlayer.classList.remove("book-section-video-player--hidden");
+            fullscreenBtn.classList.add("media-viewer__fullscreen-btn--hidden");
         } else if(mediaType == "img") {
             videoPlayer.classList.add("book-section-video-player--hidden");
             compSlider.classList.remove("comp-slider--hidden");
+            fullscreenBtn.classList.remove("media-viewer__fullscreen-btn--hidden");
         }
     };
 
+    let toggleFullscreenBtnState = function(event) {
+        let ariaLabel = fullscreenBtn.getAttribute("aria-label");
+        let enterFsIcon = fullscreenBtn.querySelector(".media-viewer__enter-fs-icon");
+        let exitFsIcon = fullscreenBtn.querySelector(".media-viewer__exit-fs-icon");
+        let labelString;
+        if(ariaLabel == "Enter full screen") {
+            labelString = "Exit full screen";
+            enterFsIcon.classList.add("media-viewer__fullscreen-btn-icon--hidden");
+            exitFsIcon.classList.remove("media-viewer__fullscreen-btn-icon--hidden");
+        } else {
+            exitFsIcon.classList.add("media-viewer__fullscreen-btn-icon--hidden");
+            enterFsIcon.classList.remove("media-viewer__fullscreen-btn-icon--hidden");
+            labelString = "Enter full screen";
+        }
+        fullscreenBtn.setAttribute("aria-label", labelString);
+        fullscreenBtn.setAttribute("title", labelString);
+    };
+
     if(tabContainer) tabContainer.addEventListener("click", handleTabContainerClick);
+    fullscreenBtn.addEventListener("click", toggleFullscreenBtnState)
 
     return {
         root,
+        fullscreenBtn,
         displayMediaType
     }
 };
