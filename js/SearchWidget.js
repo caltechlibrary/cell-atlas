b4f665fd30013e09f2e0e24b6e77a1ccd69da2a5
KianBadie <kianbadie@gmail.com>
2021-10-14 08:59:12 -0700

    Add functions to parse/format result data and create result elements in search widget.

--- js/SearchWidget.js
+++ js/SearchWidget.js
@@ -3,7 +3,9 @@ let SearchWidget = function(root) {
     let openBtn = root.querySelector(".search-widget__open-btn");
     let searchBar = root.querySelector(".search-widget__search-bar");
     let searchBarInput = root.querySelector(".search-widget__search-bar-input");
-    let index, searchTimeout;
+    let maxResults = 15;
+    let maxResultChars = 150;
+    let searchData, index, searchTimeout;
 
     let init = function() {
         fetch("searchData.json")
@@ -12,6 +14,7 @@ let SearchWidget = function(root) {
     };
 
     let initSearchFunctionality = function(data) {
+        searchData = data;
         index = lunr(function() {
             this.ref("id");
             this.field("title");
@@ -41,7 +44,109 @@ let SearchWidget = function(root) {
             query.term(queryTokens, { editDistance: 1 });
         });
 
-        console.log(resultData);
+        displayResults(resultData);
+    };
+
+    let displayResults = function(resultData) {
+        let resultsNum = (resultData.length < maxResults) ? resultData.length : maxResults;
+        for(let i = 0; i < resultsNum; i++) {
+            let formattedResultEls = getFormattedResultEls(resultData[i], searchData[resultData[i].ref]);
+            console.log(formattedResultEls);
+        }
+    };
+
+    let getFormattedResultEls = function(resultData, sourceDoc) {
+        let formattedHighlightData = getFormattedHighlightData(resultData.matchData.metadata);
+        let formattedResultEls = {};
+        for(field in sourceDoc) {
+            if(formattedHighlightData[field]) {
+                let truncatedResources = getTruncatedResources(formattedHighlightData[field], sourceDoc[field]);
+                formattedResultEls[field] = getFormattedHTML(truncatedResources.highlightData, truncatedResources.sourceContent);
+            } else {
+                let truncatedSourceContent = (sourceDoc[field].length > maxResultChars) ? `${sourceDoc[field].substring(0, maxResultChars)}...` : sourceDoc[field];
+                let resultEl = document.createElement("span");
+                let sourceContentTextNode = document.createTextNode(truncatedSourceContent);
+                resultEl.appendChild(sourceContentTextNode);
+                formattedResultEls[field] = resultEl;
+            }
+        }
+        return formattedResultEls;
+    };
+
+    let getFormattedHighlightData = function(tokenPositionData) {
+        let highlightData = {};
+        for(let token in tokenPositionData) {
+            let positionData = tokenPositionData[token];
+            for(let field in positionData) {
+                if(highlightData[field]) {
+                    highlightData[field] = highlightData[field].concat(positionData[field].position);
+                } else {
+                    highlightData[field] = [].concat(positionData[field].position);
+                }
+            }
+        }
+        for(field in highlightData) highlightData[field].sort(function(a, b) { return a[0] - b[0] });
+        return highlightData;
+    };
+
+    let getTruncatedResources = function(highlightData, sourceContent) {
+        if(sourceContent.length <= maxResultChars) return { highlightData, sourceContent };
+        let highlightStart = highlightData[0][0];
+        let highlightStartLength = highlightData[0][1];
+        let surroundingCharCount = Math.floor((maxResultChars - highlightStartLength) / 2);
+        let truncatedHighlightData, truncatedSourceString;
+        if(highlightStart < surroundingCharCount) {
+            // highlighted word should not have beggining truncated
+            truncatedSourceString = `${sourceContent.substring(0, maxResultChars)}...`;
+            truncatedHighlightData = highlightData.filter(function(highlightDatum) { 
+                return (highlightDatum[0] + highlightDatum[1]) < maxResultChars;
+            });
+        } else if(highlightStart > sourceContent.length - surroundingCharCount) {
+            // highlihgted word should not have end truncated
+            let truncatedStart = sourceContent.length - maxResultChars;
+            truncatedSourceString = `...${sourceContent.substring(truncatedStart)}`;
+            truncatedHighlightData = highlightData.filter(function(highlightDatum) { 
+                return highlightDatum[0] > truncatedStart;
+            }).map(function(highlightDatum) {
+                return [(highlightDatum[0] - truncatedStart) + 3, highlightDatum[1]];
+            });
+        } else {
+            // highlighted word should be truncated on both sides
+            let truncatedStart = highlightStart - surroundingCharCount;
+            let truncatedEnd = highlightStart + highlightStartLength + surroundingCharCount;
+            truncatedSourceString = `...${sourceContent.substring(truncatedStart, truncatedEnd)}...`;
+            truncatedHighlightData = highlightData.filter(function(highlightDatum) { 
+                return highlightDatum[0] > truncatedStart && (highlightDatum[0] + highlightDatum[1]) < truncatedEnd;
+            }).map(function(highlightDatum) {
+                return [(highlightDatum[0] - truncatedStart) + 3, highlightDatum[1]];
+            });
+        }
+        return { highlightData: truncatedHighlightData, sourceContent: truncatedSourceString };
+    };
+
+    let getFormattedHTML = function(highlightData, sourceContent) {
+        let outerEl = document.createElement("span");
+        for(let i = 0; i < highlightData.length; i++) {
+            let prevData = highlightData[i - 1];
+            let startPos = (prevData) ? prevData[0] + prevData[1] : 0;
+            let highlightStartPos = highlightData[i][0];
+            let highlightEndPos = highlightStartPos + highlightData[i][1];
+            let preText = sourceContent.substring(startPos, highlightStartPos);
+            let preTextNode = document.createTextNode(preText);
+            let highlightText =  sourceContent.substring(highlightStartPos, highlightEndPos);
+            let highlightTextNode = document.createTextNode(highlightText);
+            let highlightedSpan = document.createElement("span");
+            highlightedSpan.classList.add("search-widget__result-highlight");
+            highlightedSpan.appendChild(highlightTextNode);
+            outerEl.appendChild(preTextNode);
+            outerEl.appendChild(highlightedSpan);
+            if(i == highlightData.length - 1) {
+                let trailingText = sourceContent.substring(highlightEndPos);
+                let trailingTextNode = document.createTextNode(trailingText)
+                outerEl.appendChild(trailingTextNode);
+            }
+        }
+        return outerEl;
     };
 
     let handleOpenBtnClick = function(event) {
