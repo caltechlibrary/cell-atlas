3b9a79413fccfd07478d6a507bde37ea87c93107
KianBadie <kianbadie@gmail.com>
2021-11-16 15:53:20 -0800

    Add nav menu widget and chapter controller to chapter-refactor script.

--- js/chapter-refactor.js
+++ js/chapter-refactor.js
@@ -1,7 +1,36 @@
 (function() {
+    let navEl = document.querySelector(".nav");
+    let navBtn = document.querySelector(".header__nav-btn");
     let progressBarEl = document.querySelector(".progress-bar");
     let footerEl = document.querySelector(".footer");
     let progressBar, footer;
+
+    let ChapterController = function() {
+
+        let toggleNav = function() {
+            if(nav.root.classList.contains("nav--collapsed")) {
+                nav.show();
+                nav.root.querySelector("a").focus();
+                window.addEventListener("click", clickCloseNav);
+                window.addEventListener("keydown", keydownCloseNav);
+            } else {
+                nav.hide();
+                navBtn.focus();
+                window.removeEventListener("click", clickCloseNav);
+                window.removeEventListener("keydown", keydownCloseNav);
+            }
+        };
+
+        return {
+            toggleNav
+        };
+
+    };
+
+    let chapterController = ChapterController();
+
+    nav = NavWidget(navEl);
+    navBtn.addEventListener("click", chapterController.toggleNav);
 
     progressBar = ProgressBar(progressBarEl);
 
