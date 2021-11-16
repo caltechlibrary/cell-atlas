0bb4bd91bef37ac2ab9d849b50b595ccc6ee1987
KianBadie <kianbadie@gmail.com>
2021-11-15 16:51:52 -0800

    Add functionality to use auto height on section lists.

--- js/NavWidget.js
+++ js/NavWidget.js
@@ -26,11 +26,19 @@ let NavWidget = function(root) {
     let expandSectionList = function(sectionList) {
         sectionList.classList.remove("nav__section-list--collapsed");
         sectionList.style.height = `${sectionList.scrollHeight}px`;
+        sectionList.addEventListener("transitionend", onSectionListExpanded, { once: true });
+    };
+
+    let onSectionListExpanded = function(event) {
+        if(!event.target.classList.contains("nav__section-list--collapsed")) event.target.style.height = null;
     };
 
     let collapseSectionList = function(sectionList) {
-        sectionList.classList.add("nav__section-list--collapsed");
-        sectionList.style.height = "0px";
+        sectionList.style.height = `${sectionList.scrollHeight}px`;
+        window.requestAnimationFrame(() => {
+            sectionList.classList.add("nav__section-list--collapsed");
+            sectionList.style.height = "0px";
+        });
     };
 
     let show = function() {
