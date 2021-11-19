bc8147ea2a540acae7b1afc5b8b9bbeca3042602
KianBadie <kianbadie@gmail.com>
2021-11-19 11:23:52 -0800

    Remove protein number from atom label.

--- js/ProteinViewer.js
+++ js/ProteinViewer.js
@@ -40,17 +40,6 @@ let ProteinViewer = function(root) {
         "HOH": "water",
         "XAA": "other"
     };
-    let proteinNumDict = {
-        "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6,"G": 7,
-        "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14,
-        "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21,
-        "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26, "a": 27, "b": 28,
-        "c": 29, "d": 30, "e": 31, "f": 32, "g": 33, "h": 34, "i": 35,
-        "j": 36, "k": 37, "l": 38, "m": 39, "n": 40, "o": 41, "p": 42, 
-        "q": 43, "r": 44, "s": 45, "t": 46, "u": 47, "v": 48, "w": 49,
-        "x": 50, "y": 51, "z": 52, "1": 53, "2": 54, "3": 55, "4": 56,
-        "5": 57, "6": 58, "7": 59, "8": 60, "9": 61
-    };
     let viewerStructs, prevPicked;;
 
     let init = function() {
@@ -133,7 +122,6 @@ let ProteinViewer = function(root) {
         if(picked) {
             let atom = picked.target();
             let atomNameComponents = atom.qualifiedName().split(".");
-            let proteinNum = proteinNumDict[atomNameComponents[0]];
             let residueNum = atomNameComponents[1].substring(3);
             let proteinName = proteinDict[atomNameComponents[1].substring(0, 3)];
             highlightAtom(picked.node(), atom);
@@ -142,7 +130,7 @@ let ProteinViewer = function(root) {
             } else if(proteinName == "water") {
                 atomLabel.innerHTML = `${proteinName}`;
             } else {
-                atomLabel.innerHTML = `Protein ${proteinNum} | Residue #${residueNum} | ${proteinName}`;
+                atomLabel.innerHTML = `Residue #${residueNum} | ${proteinName}`;
             }
             atomLabel.classList.remove("protein-viewer__atom-label--hidden");
             prevPicked = { node: picked.node() }
