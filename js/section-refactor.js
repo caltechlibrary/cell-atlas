b3f2bb2c2022ce9be997f86d25afcae52df18ba2
KianBadie <KianBadie@users.noreply.github.com>
2021-11-22 17:52:46 +0000

    deploy: 77c6e1d2114f9533174ffc6513667abef09fca3b

--- js/section-refactor.js
+++ js/section-refactor.js
@@ -96,37 +96,15 @@
         };
 
         let shelveTextWidget = function() {
-            sectionText.setMainTabIndex(-1);
-
-            // Add classess to determine visual order that transitions happen
-            sectionText.root.classList.remove("section-text--transition-delay");
-            sectionText.root.classList.add("section-text--transition-instant");
-            sectionText.mainContainer.classList.remove("section-text__main-container-transition-instant");
-            sectionText.mainContainer.classList.add("section-text__unshelve-btn-container--transition-delay");
-            sectionText.unshelveBtnContainer.classList.remove("section-text__unshelve-btn-container--transition-instant");
-            sectionText.unshelveBtnContainer.classList.add("section-text__unshelve-btn-container--transition-delay");
-
             sectionText.root.classList.add("section-text--shelved");
             sectionText.mainContainer.classList.add("section-text__main-container--hidden");
             sectionText.unshelveBtnContainer.classList.remove("section-text__unshelve-btn-container--hidden");
-            sectionText.unshelveBtnContainer.setAttribute("tabindex", 0);
         };
 
         let unShelveTextWidget = function() {
-            sectionText.unshelveBtnContainer.setAttribute("tabindex", -1);
-
-            // Add classess to determine visual order that transitions happen
-            sectionText.root.classList.remove("section-text--transition-instant");
-            sectionText.root.classList.add("section-text--transition-delay");
-            sectionText.mainContainer.classList.remove("section-text__main-container-transition-instant");
-            sectionText.mainContainer.classList.add("section-text__unshelve-btn-container--transition-delay");
-            sectionText.unshelveBtnContainer.classList.remove("section-text__unshelve-btn-container--transition-delay");
-            sectionText.unshelveBtnContainer.classList.add("section-text__unshelve-btn-container--transition-instant");
-            
             sectionText.unshelveBtnContainer.classList.add("section-text__unshelve-btn-container--hidden");
             sectionText.mainContainer.classList.remove("section-text__main-container--hidden");
             sectionText.root.classList.remove("section-text--shelved");
-            sectionText.setMainTabIndex(0);
         };
 
         let handleLearnMoreBtnContainerClick = function(event) {
