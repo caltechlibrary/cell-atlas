8761bc4d87cb270290f7c77a8d419a71c4714a1e
KianBadie <kianbadie@gmail.com>
2021-12-02 14:39:18 -0800

    Add touch/mouse panning and pinch zooming to refactored tree viewer.

--- js/TreeViewer.js
+++ js/TreeViewer.js
@@ -1,8 +1,17 @@
 let TreeViewer = function(root) {
 
+    let svgContainer = root.querySelector(".tree-viewer__svg-container");
+    let treeSvg = root.querySelector(".tree-viewer__tree-svg");
     let speciesAnchors = root.querySelectorAll(".tree-viewer__species-anchor");
     let popUps = root.querySelectorAll(".tree-viewer__pop-up");
     let deactivatePopUp;
+    svgContainer.treeSvg = treeSvg;
+    svgContainer.eventCache = [];
+    svgContainer.prevDiff = -1;
+    svgContainer.curScale = 1;
+    svgContainer.curTranslateX = 0;
+    svgContainer.curTranslateY = 0;
+    svgContainer.zoomWeight = 1.025;
 
     let onSpeciesAnchorFocus = function(event) {
         let speciesAnchor = event.currentTarget;
@@ -34,6 +43,76 @@ let TreeViewer = function(root) {
         if(popUp) popUp.classList.add("tree-viewer__pop-up--hidden");
     };
 
+    let onPointerdown = function(event) {
+        this.eventCache.push(event);
+    };
+
+    let onPointermove = function(event) {
+        if(this.eventCache.length == 0) return;
+        let prevGridPos1 = calcGridPos(this.eventCache[0].clientX, this.eventCache[0].clientY);
+        let zoomFactor = 1;
+        let gridPos1, gridPos2, curDiff, midPoint;
+
+        for (let i = 0; i < this.eventCache.length; i++) {
+            if (event.pointerId == this.eventCache[i].pointerId) {
+                this.eventCache[i] = event;
+                break;
+            }
+        }
+        
+        gridPos1 = calcGridPos(this.eventCache[0].clientX, this.eventCache[0].clientY);
+
+        this.curTranslateX = this.curTranslateX - (prevGridPos1.posX - gridPos1.posX);
+        this.curTranslateY = this.curTranslateY - (gridPos1.posY - prevGridPos1.posY);
+
+        if (this.eventCache.length == 2) {
+            gridPos2 = calcGridPos(this.eventCache[1].clientX, this.eventCache[1].clientY);
+            curDiff = Math.hypot(gridPos1.posX - gridPos2.posX, gridPos1.posY - gridPos2.posY);
+            midPoint = {
+                posX: (gridPos1.posX + gridPos2.posX) / 2,
+                posY: (gridPos1.posY + gridPos2.posY) / 2,
+            };
+            
+            if (this.prevDiff > 0) {
+                if (curDiff > this.prevDiff) {
+                    zoomFactor = this.zoomWeight;
+                }
+                if (curDiff < this.prevDiff) {
+                    zoomFactor = (1 / this.zoomWeight);
+                }
+            }
+
+            this.curTranslateX = this.curTranslateX - ( (midPoint.posX - this.curTranslateX) * (zoomFactor - 1) );
+            this.curTranslateY = this.curTranslateY - ( (this.curTranslateY - midPoint.posY) * (zoomFactor - 1) );
+            this.curScale = this.curScale * zoomFactor;
+            
+            this.prevDiff = curDiff;
+        }
+
+        this.treeSvg.style.transform = `matrix(${this.curScale}, 0, 0, ${this.curScale}, ${this.curTranslateX}, ${this.curTranslateY})`;
+    };
+
+    let onPointerup = function(event) {
+        for (let i = 0; i < this.eventCache.length; i++) {
+            if (this.eventCache[i].pointerId == event.pointerId) {
+                this.eventCache.splice(i, 1);
+                break;
+            }
+        }
+
+        if (this.eventCache.length < 2) {
+            this.prevDiff = -1;
+        }
+    };
+
+    let calcGridPos = function(pageX, pageY) {
+        let centerX = (root.getBoundingClientRect().right - root.getBoundingClientRect().x) / 2;
+        let centerY = (root.getBoundingClientRect().bottom - root.getBoundingClientRect().y) / 2;
+        let posX = (pageX - root.getBoundingClientRect().x) - centerX;
+        let posY = centerY - (pageY - root.getBoundingClientRect().y);
+        return { posX, posY };
+    };
+
     for(let speciesAnchor of speciesAnchors) {
         speciesAnchor.addEventListener("mouseenter", onSpeciesAnchorFocus);
         speciesAnchor.addEventListener("mouseleave", initSpeciesEntryDeactivation);
@@ -42,6 +121,12 @@ let TreeViewer = function(root) {
         popUp.addEventListener("mouseenter", onPopUpFocus);
         popUp.addEventListener("mouseleave", initSpeciesEntryDeactivation);
     }
+    svgContainer.addEventListener("pointerdown", onPointerdown);
+    svgContainer.addEventListener("pointermove", onPointermove);
+    svgContainer.addEventListener("pointerup", onPointerup);
+    svgContainer.addEventListener("pointercancel", onPointerup);
+    svgContainer.addEventListener("pointerleave", onPointerup);
+
 
     return {
         root
