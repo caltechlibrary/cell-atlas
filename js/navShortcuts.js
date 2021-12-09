1fc225addf32458aaef978e05939768bae40cb28
KianBadie <KianBadie@users.noreply.github.com>
2021-12-09 22:20:44 +0000

    deploy: 6b1eea78d18177df224a9bb94e49c20d257de57a

---
+++ js/navShortcuts.js
@@ -0,0 +1,48 @@
+(function() {
+
+    let maxSwipeTime = 200;
+    let minSwipeDist = 150;
+    let vertSwipeThreshold = 100;
+    let blacklistedSwipeEls = [".protein-viewer", ".tree-viewer", ".summary-menu", ".comp-slider__slider", ".media-viewer--fullscreen-polyfill"];
+    let touchStartTime, touchStartX, touchStartY;
+
+    let onDocumentKeydown = function(event) {
+        if((event.key != "ArrowLeft" && event.key != "ArrowRight") || event.target.tagName == "INPUT") return;
+        redirectPage((event.key == "ArrowLeft") ? "prev" : "next");
+    };
+
+    let redirectPage = function(page) {
+        let link = document.querySelector(`.nav-arrow[data-page='${page}']`);
+        if(link) window.location.assign(link.getAttribute("href"));
+    };
+
+    let onTouchstart = function(event) {
+        touchStartTime = Date.now();
+        touchStartX = event.changedTouches[0].clientX;
+        touchStartY = event.changedTouches[0].clientY;
+    };
+
+    let onTouchend = function(event) {
+        let elapsedTime = Date.now() - touchStartTime;
+        let distX = touchStartX - event.changedTouches[0].clientX;
+        let distY = touchStartY - event.changedTouches[0].clientY;
+        if(validateSwipe(event.changedTouches[0], distX, distY, elapsedTime)) redirectPage((distX < 0) ? "prev" : "next");
+    };
+
+    let validateSwipe = function(touchObj, distX, distY, elapsedTime) {
+        if(document.fullscreenElement) return false;
+        if(touchObj.target.tagName == "INPUT") return false;
+        for(let blacklistedSwipeEl of blacklistedSwipeEls) {
+            if(touchObj.target.closest(blacklistedSwipeEl)) return false;
+        }
+        if(elapsedTime > maxSwipeTime) return false;
+        if(Math.abs(distX) < minSwipeDist) return false;
+        if(Math.abs(distY) > vertSwipeThreshold) return false;
+        return true;
+    };
+
+    document.addEventListener("keydown", onDocumentKeydown);
+    document.addEventListener("touchstart", onTouchstart);
+    document.addEventListener("touchend", onTouchend);
+
+})();
--- js/section.js
+++ js/section.js
@@ -9,60 +9,6 @@ for(let comparisonVideoButton of comparisonVideoButtons) {
     addTypeFocusToggle(comparisonVideoButton);
 }
 
-document.addEventListener("keydown", function(event) {
-    let focusedElement = document.activeElement;
-
-    if(event.key === "ArrowLeft" || event.key === "ArrowRight") {
-        if(focusedElement.tagName == "INPUT") return;
-        let direction = (event.key === "ArrowLeft") ? "prev" : "next" ;
-        let link = document.querySelector(`a[data-nav='${direction}']`);
-        if(link) link.click();
-    } else if(event.key == "ArrowUp" || event.key == "ArrowDown") {
-        if(focusedElement.tagName == "INPUT") return;
-        let textMaterial = document.querySelector(".section-text__content");
-        let modalOverlay = document.getElementById("modalOverlay");
-        if(modalOverlay && modalOverlay.style.display == "block") {
-            let modalContainers = document.getElementsByClassName("subsection-modal-container");
-            for(let modalContainer of modalContainers) {
-                if(modalContainer.style.display == "flex") {
-                    let modalMaterial = modalContainer.querySelector(".subsection-modal-text");
-                    modalMaterial.focus();
-                }
-            }
-        } else if(textMaterial && textMaterial.getAttribute("tabindex") == "0") {
-            textMaterial.focus();
-        }
-    } else if(event.key == " ") {
-        if(focusedElement.tagName == "BUTTON" || focusedElement.type == "checkbox" || focusedElement.type == "search") return;
-        let modalOverlay = document.getElementById("modalOverlay");
-        let nonTextContent = document.querySelector(".main-non-text-container");
-        let videoPlayer;
-        if(modalOverlay && modalOverlay.style.display == "block") {
-            let modalContainers = document.getElementsByClassName("subsection-modal-container");
-            for(let modalContainer of modalContainers) {
-                if(modalContainer.style.display == "flex") {
-                    let modalText = modalContainer.querySelector(".subsection-modal-text");
-                    if(modalText.contains(focusedElement)) return;
-                    videoPlayer = modalContainer.querySelector(".book-section-video-player");
-                }
-            }
-        } else if(nonTextContent) {
-            let textMaterial = document.querySelector(".section-text__content");
-            if(textMaterial.contains(focusedElement)) return;
-            videoPlayer = nonTextContent.querySelector(".book-section-video-player");
-        }
-        if(videoPlayer) {
-            let playerId = videoPlayer.getAttribute("data-player");
-            let selectedTab = document.querySelector(`.book-section-comparison-button-container button[data-player='${playerId}'][data-state='selected']`);
-            let videoTab = document.querySelector(`.book-section-comparison-button-container button[data-player='${playerId}'][value='video']`);
-            if(!videoTab || (selectedTab && selectedTab.value == "video")) {
-                let playPauseButton = document.getElementById(`${playerId}-playPauseButton`);
-                playPauseButton.click();
-            }
-        }
-    }
-});
-
 // Check url to see if modal anchor is included. If so, trigger button press
 let secUrl = window.location.href;
 let secSplit = secUrl.split("#");
