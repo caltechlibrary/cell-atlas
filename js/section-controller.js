1a526b9f4e809e929e5ab8b2a14c3b9deb7dd0fe
KianBadie <KianBadie@users.noreply.github.com>
2021-12-17 22:20:47 +0000

    deploy: 65b3f9d3fd47b316b7b438171bf63e5fc55cc0b5

--- js/section-controller.js
+++ js/section-controller.js
@@ -200,7 +200,7 @@
     mainMediaViewer.fullscreenBtn.addEventListener("click", sectionController.handleMainMediaViewerFsBtnClick);
     if(mainMediaViewer.videoPlayer) mainMediaViewer.videoPlayer.video.addEventListener("play", sectionController.onMainVideoPlayerFirstPlay, { once: true });
     
-    if(window.createImageBitmap && mainMediaViewer.videoPlayer) mainNonTextContainer.addEventListener("transitionend", sectionController.resizeMainPlayerScrubCanvas);
+    // if(window.createImageBitmap && mainMediaViewer.videoPlayer) mainNonTextContainer.addEventListener("transitionend", sectionController.resizeMainPlayerScrubCanvas);
     mainStopNarrationButtonMobile.addEventListener("click", sectionController.stopMainNarration);
 
     for(let narrationPlayerEl of narrationPlayerEls) {
