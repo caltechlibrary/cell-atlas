834211422db7ef215b6fd205d8d8551a50cade5f
Kian Badie <kianbadie@gmail.com>
2022-03-31 14:12:25 -0700

    Markup modals to behave like actual modals. Add keyboard focus trapping.

--- js/Modal.js
+++ js/Modal.js
@@ -2,13 +2,17 @@ let Modal = function(root, onOpenCallback = function(){},  onCloseCallback = fun
 
     let modalContainer = root.querySelector(".modal__modal-container");
     let exitBtn = root.querySelector(".modal__exit-btn");
-    let contentContainer = root.querySelector(".modal__content-container");
+    let focusableEls = root.querySelectorAll("button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])");
+    let lastFocused;
 
     let show = function() {
-        if(contentContainer) contentContainer.setAttribute("tabindex", 0);
+        lastFocused = document.activeElement;
+        let firstFocusable = focusableEls[(window.innerWidth < 900) ? 0 : 1];
+        
         root.classList.remove("modal--hidden");
-        root.focus();
+        firstFocusable.focus();
         document.addEventListener("keydown", onOpenModalKeyDown);
+
         onOpenCallback();
     };
 
@@ -18,16 +22,29 @@ let Modal = function(root, onOpenCallback = function(){},  onCloseCallback = fun
 
     let hide = function() {
         onCloseCallback();
+
         root.classList.add("modal--hidden");
-        if(contentContainer) contentContainer.setAttribute("tabindex", -1);
         document.removeEventListener("keydown", onOpenModalKeyDown);
+        lastFocused.focus();
     };
 
     let onRootClick = function(event) {
         if(!modalContainer.contains(event.target)) hide();
     };
 
+    let onRootKeydown = function(event) {
+        if(event.key != "Tab") return;
+        if(event.target == focusableEls[focusableEls.length - 1] && !event.shiftKey) {
+            focusableEls[window.innerWidth < 900 ? 0 : 1].focus();
+            event.preventDefault();
+        } else if(event.target == focusableEls[window.innerWidth < 900 ? 0 : 1] && event.shiftKey) {
+            focusableEls[focusableEls.length - 1].focus();
+            event.preventDefault();
+        }
+    };
+
     root.addEventListener("click", onRootClick);
+    root.addEventListener("keydown", onRootKeydown);
     exitBtn.addEventListener("click", hide);
 
     return {
