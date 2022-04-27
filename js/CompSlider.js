bfcbdccb4d1e1bbc5504a47b0287bd1bd35b8972
Kian Badie <kianbadie@gmail.com>
2022-04-27 15:11:40 -0700

    Remove comp slider image load failed functionality.

--- js/CompSlider.js
+++ js/CompSlider.js
@@ -1,18 +1,8 @@
 let CompSlider = function(root) {
     
-    let beforeImg = root.querySelector(".comp-slider__before-img");
     let afterImgContainer = root.querySelector(".comp-slider__after-img-container");
-    let afterImg = root.querySelector(".comp-slider__after-img");
     let slider = root.querySelector(".comp-slider__slider");
     let sliderInput = root.querySelector(".comp-slider__input");
-    let failMsgContainer = root.querySelector(".comp-slider__fail-msg-container");
-
-    let displayFailedMsg = function(event) {
-        beforeImg.classList.add("comp-slider__before-img--hidden");
-        afterImgContainer.classList.add("comp-slider__after-img-container--hidden");
-        slider.classList.add("comp-slider__slider--hidden");
-        failMsgContainer.classList.remove("comp-slider__fail-msg-container--hidden");
-    };
 
     let initImgSliding = function(event) {
         event.preventDefault();
@@ -54,8 +44,6 @@ let CompSlider = function(root) {
         window.removeEventListener("touchend", endImgSliding);
     };
 
-    beforeImg.addEventListener("error", displayFailedMsg);
-    afterImg.addEventListener("error", displayFailedMsg);
     slider.addEventListener("mousedown", initImgSliding);
     slider.addEventListener("touchstart", initImgSliding);
     sliderInput.addEventListener("input", onSliderManualInput);
