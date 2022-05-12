7e997f8dedf87d7656845b0949bcad483c883999
Kian Badie <kianbadie@gmail.com>
2022-05-12 16:37:10 -0700

    Add documentation to VideoPlayer.js

--- js/VideoPlayer.js
+++ js/VideoPlayer.js
@@ -1,7 +1,19 @@
+/**
+ * Creates a video player widget and returns videojs player object.
+ * This widget definition looks different than other widget definitions
+ * in this project because it internally uses a different video player
+ * library, videojs https://videojs.com/.
+ *
+ * @param root The dom element being registered as a video player.
+ */
 let VideoPlayer = function(root) {
+
+    // Create frequently used variables.
     let offline = root.getAttribute("data-offline");
     let doi = root.getAttribute("data-doi");
     let vidName = root.getAttribute("data-vid-name")
+
+    // Create video player object from videojs. Returned from VideoPlayer(). 
     let videoPlayer = videojs(root, {
         controls: true,
         controlBar: {
@@ -17,6 +29,10 @@ let VideoPlayer = function(root) {
         }
     });
 
+    /**
+     * Initialize video player by setting correct source and adding
+     * custom quality changer to videojs player if online.
+     */
     let init = async function() {
         if(offline) {
             videoPlayer.src(`videos/${videoPlayer.getAttribute("data-src")}`);
@@ -25,12 +41,14 @@ let VideoPlayer = function(root) {
             let vidQuality = (window.sessionStorage.getItem("vidQuality") == "480") ? "480" : "1080";
 
             // Set variables for 1080/480 src
-            if(doi) { // If doi, generate source strings from that
+            if(doi) { 
+                // If doi, generate source strings from that
                 let res = await fetch(`https://api.datacite.org/dois/${doi}/media`);
                 let data = await res.json();
                 src1080 = data.data[0].attributes.url;
                 src480 = `${src1080.substring(0, src1080.indexOf(".mp4"))}_480p.mp4`;
-            } else { // If no doi, fallback to vidName variable which is set by build script
+            } else { 
+                // If no doi, fallback to vidName variable which is set by build script
                 src1080 = `https://www.cellstructureatlas.org/videos/${vidName}.mp4`;
                 src480 = `https://www.cellstructureatlas.org/videos/${vidName}_480p.mp4`;
             }
@@ -38,7 +56,10 @@ let VideoPlayer = function(root) {
             // Load source in video element
             videoPlayer.src((vidQuality == "480") ? src480 : src1080);
 
-            // Add quality switcher
+            // Add custom quality switcher defined in QualitySelector.js, where it is 
+            // registered as a videojs component. The third argument "13" represents
+            // the order the changer appears in the player controls (13th child) and
+            // is a part of videojs.
             videoPlayer.qualityChanger = videoPlayer.controlBar.addChild("QualityChanger", {
                 qualities: [
                     { quality: "1080", src: src1080 },
@@ -48,14 +69,24 @@ let VideoPlayer = function(root) {
         }
     };
 
+    /**
+     * Force player fullscreen when video is played on mobile screens.
+     * Fired on videojs player play event.
+     */
     let onPlay = function() {
         if(window.innerWidth < 900) videoPlayer.requestFullscreen();
     };
 
+    /**
+     * Pause player when video exits fullscreen on mobile screens.
+     * Fired on videojs fullscreenchange event.
+     */
     let onFullscreenchange = function() {
         if(window.innerWidth < 900 && !videoPlayer.isFullscreen()) videoPlayer.pause();
     };
 
+    // Init player and add event listeners (the videojs way with "on" method since 
+    // videoPlayer is a videojs video player).
     init();
     videoPlayer.on("play", onPlay);
     videoPlayer.on("fullscreenchange", onFullscreenchange);
