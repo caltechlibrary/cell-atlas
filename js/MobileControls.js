00edcabb30139aed2fe4afe1fd5d68ae4b772eef
Kian Badie <kianbadie@gmail.com>
2022-07-13 15:35:37 -0700

    Add documentation to MobileControls.js

--- js/MobileControls.js
+++ js/MobileControls.js
@@ -1,17 +1,36 @@
+/**
+ * Creates a mobile controls widget.
+ *
+ * @param root The dom element being registered as a mobile control.
+ * @param onControlClickCallback Callback fired when a control is clicked.
+ */
 let MobileControls = function(root, onControlClickCallback) {
 
+    // Create references to frequently used dom elements.
     let controlBtns = root.querySelectorAll(".mobile-controls__btn");
 
+    /**
+     * Called on controlBtn click. Visually updates currently 
+     * selected control and calls onControlClickCallback.
+     * 
+     * @param event click event that initiated this function.
+     */
     let handleControlsClick = function(event) {
         let controlBtn = event.currentTarget;
         let currSelected = root.querySelector(".mobile-controls__btn--selected");
 
+        // If clicked button is currently selected, return.
         if(controlBtn == currSelected) return;
+
+        // Update classes for prev/curr selected controls.
         currSelected.classList.remove("mobile-controls__btn--selected");
         controlBtn.classList.add("mobile-controls__btn--selected");
+
+        // Pass curr selected control value to callback.
         onControlClickCallback(controlBtn.value);
     };
 
+    // Add neccessary event listeners to dom elements.
     for(let controlBtn of controlBtns) controlBtn.addEventListener("click", handleControlsClick);
 
 };
