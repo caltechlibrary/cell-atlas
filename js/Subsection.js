64e09f49ba87cea18d2e8001ff52698539982d3e
Kian Badie <kianbadie@gmail.com>
2022-08-08 14:33:00 -0700

    Add documentation to Subsection.js

--- js/Subsection.js
+++ js/Subsection.js
@@ -1,9 +1,26 @@
+/**
+ * Turns a dom element into a subsection component.
+ *
+ * @param root The dom element being registered as a subsection component.
+ * @param onNarrationOpenCallback Callback called when narration player is shown.
+ * @param onNarrationCloseCallback Callback called when narration player is hidden.
+ */
 let Subsection = function(root, onNarrationOpenCallback = function(){},  onNarrationCloseCallback = function(){}) {
 
+    // Create references to frequently used dom elements.
     let narrationPlayerEl = root.querySelector(".subsection__narration-player");
     let narrationToggleBtn = root.querySelector(".subsection__toggle-narration-btn");
 
+    /**
+     * Called on narrationToggleBtn click. Shows/hides narration player 
+     * subcomponent.
+     */
     let toggleNarrationPlayer = function() {
+        /**
+         * This component can be better decoupled by making narration player a bem element of subsection.
+         * So managing classes like subsection___narration-player.
+         */
+
         if(narrationPlayerEl.classList.contains("narration-player--hidden")) {
             narrationPlayerEl.classList.remove("narration-player--hidden");
             narrationToggleBtn.classList.add("subsection__toggle-narration-btn--activated");
@@ -17,5 +34,6 @@ let Subsection = function(root, onNarrationOpenCallback = function(){},  onNarra
         }
     };
 
+    // Add neccessary event listeners to dom elements.
     narrationToggleBtn.addEventListener("click", toggleNarrationPlayer);
 };
